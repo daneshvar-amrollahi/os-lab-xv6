d12e012923bad45d5f38a238ebf289d54ac0c36b
Alireza Tavakoli <alirezata3akoli@gmail.com>
2021-03-02 21:57:43 +0330

    Add our names.

--- xv6-public/init.c
+++ xv6-public/init.c
@@ -20,7 +20,7 @@ main(void)
   dup(0);  // stderr
 
   for(;;){
-    printf(1, "init: starting sh\n");
+    printf(1, "init: starting sh\nAmin Setayesh\nDaneshvar Amrollahi\nAlireza Tavakoli\n");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
